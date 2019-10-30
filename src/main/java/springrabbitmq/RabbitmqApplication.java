ce11a3aae2771550dc96940c31be62ba6edc15c2
renhuan.ding <renhuan.ding@yitu-inc.com>
2019-10-30 10:46:59 +0800

    delete not proper content

--- src/main/java/springrabbitmq/RabbitmqApplication.java
+++ src/main/java/springrabbitmq/RabbitmqApplication.java
@@ -3,16 +3,38 @@ package springrabbitmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import springrabbitmq.rabbitmq.BeanTest;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class RabbitmqApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ApplicationContext context = SpringApplication.run(RabbitmqApplication.class, args);
         String[] allBeanNames = context.getBeanDefinitionNames();
         for(String beanName : allBeanNames) {
             System.out.println(beanName);
         }
         Object hello = context.getBean("string");
+        System.out.println((String)hello);
+        BeanTest test = context.getBean(BeanTest.class);
+        test.testServerAddress();
+        InetAddress ip;
+        try {
+
+            ip = InetAddress.getLocalHost();
+            ip.getHostName();
+
+            System.out.println("Current hostname : " + ip.getHostName());
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+
+        }
+
         System.out.println("hello");
     }
 }
--- src/main/java/springrabbitmq/rabbitmq/BeanTest.java
+++ src/main/java/springrabbitmq/rabbitmq/BeanTest.java
@@ -1,13 +1,43 @@
 package springrabbitmq.rabbitmq;
 
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Component
 public class BeanTest {
+    @Value("${local.server.address:0}")
+    private String serverAddress;
+    @Value("${server.port:0}")
+    private String serverPort;
+    @Autowired
+    Environment environment;
+
+
     @Bean
     public String string() {
-        return "hello bean";
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "localhost-"+ RandomStringUtils.random(10, true, true);
+        }
+    }
+    @Bean
+    public String string1() {
+        return "string1";
+    }
+
+    public void testServerAddress() throws Exception{
+        System.out.println("server addr: " + InetAddress.getLocalHost().getHostAddress());
+        System.out.println("server port: " + serverPort);
+        System.out.println(environment.getProperty("server.port"));
     }
 }
